f3e6269d4d754b2774da1f04b37e5b1ebcd3a9bd
CanhhnaC <nguyentrancanh81@gmail.com>
2020-05-17 20:11:05 +0700

    Sieve of Erathosthenes

---
+++ 2, Classic Alogrithms/Sieve of Eratosthenes.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+# Use number
+
+
+def sieveOfEratosthenes(n):
+    arr = []
+    for i in range(2, n + 1):
+        if i not in arr:
+            print(i)
+            for j in range(i * i, n + 1, i):
+                arr.append(j)
+
+
+if __name__ == "__main__":
+    n = int(input('n= '))
+    sieveOfEratosthenes(n)
