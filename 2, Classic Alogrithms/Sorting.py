a44a26f0a1a9c84bb41cab9527732b4de25cbeed
CanhhnaC <nguyentrancanh81@gmail.com>
2020-05-17 17:17:46 -0700

    Sorting

---
+++ 2, Classic Alogrithms/Sorting.py
@@ -0,0 +1,56 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if swapped == False:
+            break
+
+
+def inputArr(arr):
+    n = int(input('Number of array: '))
+    for i in range(0, n):
+        print('[', i, ']= ', end='')
+        arr.append(int(input()))
+
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements
+        R = arr[mid:]  # into 2 halves
+
+        mergeSort(L)  # Sorting the first half
+        mergeSort(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    mergeSort(arr)
+    print(arr)
